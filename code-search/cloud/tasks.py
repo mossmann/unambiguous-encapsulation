2ce66b8d830829bd2a5f3cf587b524a79350e9bf
Dominic Spill <dominicgs@gmail.com>
2014-10-30 18:33:15 +0000

    Minor update to the cloud task dispatcher  - Committing a change I made a few days ago

--- code-search/cloud/tasks.py
+++ code-search/cloud/tasks.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 
 # Initial run
-for x in range(2, 16):
+for x in range(16, 17):
 	for y in range(1, x):
 		for z in range(y+1, x):
-			print "icblbc -l %d %d %d" % (x, y, z)
+			print "icblbc %d %d %d" % (x, y, z)
 
-for x in range(2, 16):
+for x in range(16, 17):
 	for y in range(1, x):
 		print "lcbbc %d %d" % (x, y)
 
-for x in range(2, 20):
-	for y in range(1, 40):
-		for z in range(y+1, 60):
-			print "icnbc -l %d %d %d" % (x, y, z)
+for x in range(20, 21):
+	for y in range(1, 41):
+		for z in range(y+1, 61):
+			print "icnbc %d %d %d" % (x, y, z)
